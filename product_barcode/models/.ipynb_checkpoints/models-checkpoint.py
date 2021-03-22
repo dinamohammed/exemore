0581d89fdd6de92cd8ef8f7d09cf515001746de6
dinamohammed <dina.mohammed392@gmail.com>
2021-03-22 16:10:44 +0000

    Add Barcode generation to variants

--- product_barcode/models/.ipynb_checkpoints/models-checkpoint.py
+++ product_barcode/models/.ipynb_checkpoints/models-checkpoint.py
@@ -9,8 +9,7 @@ from odoo.osv import expression
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
     
-    
-    @api.onchange('seller_ids')
+    @api.onchange('seller_ids','id')
     @api.depends('seller_ids')
     def _generate_product_code(self):
         """
@@ -19,15 +18,14 @@ class ProductTemplate(models.Model):
         Vendor id from field [vendor id] [4 digits]
 		"""
         for template in self:
-            if template.seller_ids[0].id:
-                vendor_id = template.seller_ids[0].id
-                template_id = template.id
-                template.barcode = "%s%s" % (template_id.zfill(5)), vendor_id.zfill(4)))
+            if template.seller_ids:
+                vendor_id = template.seller_ids[0].name.id
+                template_id = self.search([],order='id desc', limit = 1)
+                next_id = template_id.id + 1
+                template.barcode = '"%s""%s"' % (str(next_id).zfill(5), str(vendor_id).zfill(4))
             else:
                 template.barcode = ""
-    
-    
-    
+                
 class ProductProduct(models.Model):
     _inherit = 'product.product'
     
@@ -37,9 +35,9 @@ class ProductProduct(models.Model):
     color_code = fields.Char(compute='_generate_product_code', size=3,
                              help="Color Code from field [Variant Color Attribute] [3 digits]")
     
-    @api.onchange('seller_ids', 'product_tmpl_id',
+    @api.onchange('variant_seller_ids', 'product_tmpl_id',
                   'product_template_attribute_value_ids')
-    @api.depends('seller_ids')
+    @api.depends('variant_seller_ids','product_template_attribute_value_ids')
     def _generate_product_code(self):
         """
 		Generate code of each product using it's component
@@ -51,8 +49,9 @@ class ProductProduct(models.Model):
         for product in self:
             color_attr = self.env.ref('product.product_attribute_2')
             size_attr = self.env.ref('product_barcode.product_attribute_size')
-            if product.seller_ids[0].id:
-                vendor_id = product.seller_ids[0].id
+#             raise ValidationError('hhhhhh')
+            if product.variant_seller_ids[0].name.id:
+                vendor_id = product.variant_seller_ids[0].name.id
                 template_id = product.product_tmpl_id.id
                 
                 for attr_val_line in product.product_template_attribute_value_ids:
@@ -73,8 +72,20 @@ class ProductProduct(models.Model):
                             color_code += word
                         
                     if product.size_code or color_code:
-                        product.barcode = "%s%s%s%s" % (template_id.zfill(5)), vendor_id.zfill(4)), product.size_code, color_code)
+                        product.barcode = '"%s""%s"%s""%s"' % (str(template_id).zfill(5), 
+                                                               str(vendor_id).zfill(4), product.size_code.zfill(2),
+                                                               color_code.zfill(3))
                     else:
-                        product.barcode = "%s%s" % (template_id.zfill(5)), vendor_id.zfill(4)))
+                        product.barcode = '"%s""%s"' % (template_id.zfill(5), vendor_id.zfill(4))
             else:
                 product.barcode = ""
+    
+    
+    
+    @api.model_create_multi
+    def create(self, vals_list):
+        products = super(ProductProduct, self.with_context(create_product_product=True)).create(vals_list)
+        # `_get_variant_id_for_combination` depends on existing variants
+        products._generate_product_code()
+        self.clear_caches()
+        return products
