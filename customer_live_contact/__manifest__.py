94110d0fd83b7dcd84cdf6c95e8f80fd86e8061b
dinamohammed <dina.mohammed392@gmail.com>
2021-03-28 13:35:28 +0000

    Solve multiple issues

--- customer_live_contact/__manifest__.py
+++ customer_live_contact/__manifest__.py
@@ -25,7 +25,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'data/contact_method_data.xml',
         'views/views.xml',
         'views/res_config_settings_view.xml',
--- customer_live_contact/models/res_config_settings.py
+++ customer_live_contact/models/res_config_settings.py
@@ -6,24 +6,19 @@ from odoo import fields, models, api
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
     
-    max_days_to_follow_up = fields.Integer(string="Max Days to Follow up", default=False)
-
+    max_days_to_follow_up = fields.Integer(string="Max Days to Follow up")
+    
     @api.model
-    def set_values(self):
-        self.env['ir.config_parameter'].sudo().set_param('customer_live_contact.max_days_to_follow_up',
-                                                         self.max_days_to_follow_up)
-        res = super(ResConfigSettings, self).set_values()
-        return res
-
     def get_values(self):
         res = super(ResConfigSettings, self).get_values()
-        param = self.env['ir.config_parameter'].sudo().get_param(
-            'customer_live_contact.max_days_to_follow_up',
-            self.max_days_to_follow_up)
-        res.update(
-            max_days_to_follow_up=param
-        )
+        res['max_days_to_follow_up'] = self.env['ir.config_parameter'].\
+                                       get_param('max_days_to_follow_up', default= 0)
         return res
+
+    def set_values(self):
+        self.max_days_to_follow_up and self.env['ir.config_parameter'].\
+            set_param('max_days_to_follow_up', self.max_days_to_follow_up)
+        super(ResConfigSettings, self).set_values()
 
     
     
