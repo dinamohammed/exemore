7f7aa5cdb33b6e950f3ef4a15ee5e501f91fd361
dinamohammed <dina.mohammed392@gmail.com>
2021-03-28 13:35:28 +0000

    Solve multiple issues

--- customer_live_contact/models/.ipynb_checkpoints/models-checkpoint.py
+++ customer_live_contact/models/.ipynb_checkpoints/models-checkpoint.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, _
-from odoo.exceptions import ValidationError, AccessError
+from odoo.exceptions import ValidationError, AccessError, Warning
 
 
 class ResPartner(models.Model):
@@ -12,24 +12,30 @@ class ResPartner(models.Model):
     
     @api.onchange('company_type')
     def onchange_company_type_pricelist(self):
+        res = {}
         for contact in self:
-            if contact.company_type != contact.property_product_pricelist:
-                raise ValidationError('You must change the Pricelist to be with the same Type (Individual/Company)')
-             return {
-            'warning': {'title': _("Warning"),
-                        'message': _("It is preferable to change the Accounts of this Contact..")},
-             }
+            res['warning'] = {
+                'title':_('Small Hint'),
+                'message': _("It is preferable to change the Accounts of this Contact..")}
+            contact.is_company = (contact.company_type == 'company')
+            if contact.property_product_pricelist:
+                if contact.company_type != contact.property_product_pricelist.company_type:
+                    raise ValidationError('You must change the Pricelist to be with the same Type (Individual/Company)'
+                                          + '\n'
+                                          + 'and Accounts of current Contact..')
+            return res
+
     
 
 class ProductPricelist(models.Model):
     _inherit = 'product.pricelist'
     
     company_type = fields.Selection(string='Company Type',
-        selection=[('person', 'Individual'), ('company', 'Company'), required = 'True'])
+        selection=[('person', 'Individual'), ('company', 'Company')], required = 'True', default = 'person')
     
     
 
-class contactmethod(models.Model):
+class ContactMethod(models.Model):
     _name = "contact.method"
     _description = "Contact Methods"
 
