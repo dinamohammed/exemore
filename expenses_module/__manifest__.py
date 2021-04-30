b0c3b26eebd766c763752c6549cd014ba8b66b2f
dinamohammed <dina.mohammed392@gmail.com>
2021-04-30 13:56:10 +0000

    Add

--- expenses_module/__manifest__.py
+++ expenses_module/__manifest__.py
@@ -18,7 +18,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','account','account_accountant'],
+    'depends': ['base','account','account_accountant','contacts'],
 
     # always loaded
     'data': [
